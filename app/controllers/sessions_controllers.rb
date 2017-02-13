7ddcf30287eb3df60f2186d149614e4c80e3d79f
6akayas <sayakach6@gmail.com>
2017-02-13 17:49:03 +0800

    sign up and log in controller done

---
+++ app/controllers/sessions_controllers.rb
@@ -0,0 +1,29 @@
+enable :sessions
+
+#login in render log in form
+get '/sessions/new' do
+	erb :"sessions/login"
+end
+
+#creating/finding new sessions for new user
+post '/sessions' do
+
+	current_user = User.find_by(email: params[:email])
+	if current_user && current_user.authenticate(params[:password])
+		session[:id] = current_user.id
+		erb :"sessions/dashboard"
+	 else
+		 redirect '/'
+	end
+end
+
+
+#authenticating already signed up user
+# get '/sessions/dashboard' do
+# 	authenticate_user!
+# 	erb :'sessions/dashboard'
+# end
+
+#logout
+delete '/sessions/:id' do
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+enable :sessions
+
+# index
+get '/users' do
+  @users = User.all
+end
+
+# show
+get '/users/:id' do
+end
+
+# new sign up
+get '/users/new' do
+	erb :'static/index'
+end
+
+# create
+post '/users' do
+	user = User.new(params[:user])
+  if user.save
+    session[:id] = user.id
+    # what should happen if the user is saved?
+    erb :"sessions/dashboard"
+  else
+    # what should happen if the user keyed in invalid date?
+    # @error message to appear
+    redirect '/'
+  end
+end
+
+# edit
+post '/users/:id/edit' do
+	erb :"/users/edit"
+end
+
+# update
+post '/users/:id' do
+	redirect to '/users/:id'
+end
+
+# delete
+delete '/users/:id' do
+  erb :"sessions/temp"
+end
